98ea092b64cdde359b93977c8cbea85b23702f64
Andrew <asd>
2022-02-03 16:06:12 -0600

    read confirmation message added after saving book change

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ def change_read():
     new_read = ui.get_read_value()     
     book.read = new_read 
     book.save()
+    ui.message('You have read ' + book.title + ' by ' + book.author)
     
 
 def quit_program():
